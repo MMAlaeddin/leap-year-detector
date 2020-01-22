b8487d8f6a28215d2363af687c44e4623a9b4516
mariam alaeddin and Michelle Morin <michelle@>
2020-01-22 14:43:00 -0800

    add back-end logic for numbers divisible by 100

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 // Back-end (business) logic:
 var leapYear = function(year) {
-  if (year % 4 === 0) {
+  if (year % 100 === 0) {
+    return false;
+  } else if (year % 4 === 0) {
     return true;
-  }else {
+  } else {
     return false;
   }
 };
